29d2a6e86959ccf2726b3225042c638fc0ff160b
Raghav <raghlm10@gmail.com>
2025-02-06 22:42:30 +0530

    fix: deploy fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -15,10 +15,10 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      <Experience />
+      {/* <Experience /> */}
       <Skills />
       <Projects />
-      <Education />
+      {/* <Education /> */}
       {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </div>
