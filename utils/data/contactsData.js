8b73ad80f2c8a4bdcf773ef00acc29150f2e36c7
Raghav <raghlm10@gmail.com>
2025-02-06 23:24:15 +0530

    fix: spell

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
   email: "connectwithkishanthakur@outlook.com",
   phone: "9426837619",
-  address: "Ahemdabad, Gujarat, India ",
+  address: "Ahmedabad, Gujarat, India ",
   github: "https://github.com/kishanthakur",
   linkedIn: "https://www.linkedin.com/in/kishan-thakur-a97392182/",
   devUsername: "kishanthakur",
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Kishan Thakur. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'connectwithkishanthakur@outlook.com',
   phone: '9426837619',
-  address: 'Ahemdabad, Gujarat, India',
+  address: 'Ahmedabad, Gujarat, India',
   github: 'https://github.com/kishanthakur',
   linkedIn: 'https://www.linkedin.com/in/kishan-thakur-a97392182/',
   devUsername: "kishanthakur",
